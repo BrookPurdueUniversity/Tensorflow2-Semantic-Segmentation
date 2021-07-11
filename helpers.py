e677c449995d3488e63b2dc71c3501269f65d4e2
Brook Cheng <43509137+BrookPurdueUniversity@users.noreply.github.com>
2021-07-11 17:47:57 -0400

    utils tools

---
+++ helpers.py
@@ -0,0 +1,214 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+import os,time,cv2, sys, math
+
+#import tensorflow.contrib.slim as slim   # tf__version__=1.13.1
+import tf_slim as slim   # tf__version__=2.3.0
+
+import time, datetime
+import random
+import csv
+from sklearn.metrics import precision_score, \
+    recall_score, confusion_matrix, classification_report, \
+    accuracy_score, f1_score
+import matplotlib.image as mpg
+import matplotlib.pyplot as plt
+from prettytable import PrettyTable
+
+
+def get_label_info(csv_path):
+    """
+    Retrieve the class names and label values for the selected dataset.
+    Must be in CSV format!
+    # Arguments
+        csv_path: The file path of the class dictionairy
+        
+    # Returns
+        Two lists: one for the class names and the other for the label values
+    """
+    filename, file_extension = os.path.splitext(csv_path)
+    if not file_extension == ".csv":
+        return ValueError("File is not a CSV!")
+
+    class_names = []
+    label_values = []
+    with open(csv_path, 'r') as csvfile:
+        file_reader = csv.reader(csvfile, delimiter=',')
+        header = next(file_reader)
+        for row in file_reader:
+            class_names.append(row[0])
+            label_values.append([int(row[1]), int(row[2]), int(row[3])])
+        # print(class_dict)
+    return class_names, label_values
+
+def one_hot_it(label, label_values):
+    """
+    Convert a segmentation image label array to one-hot format
+    by replacing each pixel value with a vector of length num_classes
+    # Arguments
+        label: The 2D array segmentation image label
+        label_values
+        
+    # Returns
+        A 2D array with the same width and hieght as the input, but
+        with a depth size of num_classes
+    """
+    semantic_map = []
+    for colour in label_values:
+        equality = np.equal(label, colour)
+        class_map = np.all(equality, axis = -1)
+        semantic_map.append(class_map)
+    semantic_map = np.stack(semantic_map, axis=-1)
+
+    return semantic_map
+    
+def reverse_one_hot(image):
+    """
+    Transform a 2D array in one-hot format (depth is num_classes),
+    to a 2D array with only 1 channel, where each pixel value is
+    the classified class key.
+    # Arguments
+        image: The one-hot format image 
+        
+    # Returns
+        A 2D array with the same width and height as the input, but
+        with a depth size of 1, where each pixel value is the classified 
+        class key.
+    """
+    x = np.argmax(image, axis = -1)
+    return x
+
+def colour_code_segmentation(image, label_values):
+    """
+    Given a 1-channel array of class keys, colour code the segmentation results.
+    # Arguments
+        image: single channel array where each value represents the class key.
+        label_values
+    
+    :image: 2-D, image.shape=(H,W)
+        
+    # Returns
+        Colour coded image for segmentation visualization
+    """  
+    colour_codes = np.array(label_values)
+    x = colour_codes[image.astype(int)]
+
+    return x
+
+# Get a list of the training, validation, and testing file paths
+def prepare_data(dataset_dir):
+    input_names=[]
+    output_names=[]
+    for file in os.listdir(dataset_dir + "/train_image"):
+        input_names.append(dataset_dir + "/train_image/" + file)
+    for file in os.listdir(dataset_dir + "/train_label"):
+        output_names.append(dataset_dir + "/train_label/" + file)
+    input_names.sort(),output_names.sort()
+    return input_names,output_names
+
+# read images
+def load_image(path):
+    image = cv2.cvtColor(cv2.imread(path,-1), cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (512,512))
+    return image
+
+# Takes an absolute file path and returns the name of the file without th extension
+def filepath_to_name(full_name):
+    file_name = os.path.basename(full_name)
+    file_name = os.path.splitext(file_name)[0]
+    return file_name
+
+# Print with time. To console or file
+def LOG(X, f=None):
+    time_stamp = datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+    if not f:
+        print(time_stamp + " " + X)
+    else:
+        f.write(time_stamp + " " + X)
+
+
+# Count total number of parameters in the model
+def count_params():
+    total_parameters = 0
+    for variable in tf.trainable_variables():
+        shape = variable.get_shape()
+        variable_parameters = 1
+        for dim in shape:
+            variable_parameters *= dim.value
+        total_parameters += variable_parameters
+    print("This model has %d trainable parameters"% (total_parameters))
+
+# Compute the average segmentation accuracy across all classes
+def compute_global_accuracy(pred, label):
+    total = len(label)
+    count = 0.0
+    for i in range(total):
+        if pred[i] == label[i]:
+            count = count + 1.0
+    return float(count) / float(total)
+
+# Compute the class-specific segmentation accuracy
+def compute_class_accuracies(pred, label, num_classes):
+    total = []
+    for val in range(num_classes):
+        total.append((label == val).sum())
+
+    count = [0.0] * num_classes
+    for i in range(len(label)):
+        if pred[i] == label[i]:
+            count[int(pred[i])] = count[int(pred[i])] + 1.0
+
+    # If there are no pixels from a certain class in the GT, 
+    # it returns NAN because of divide by zero
+    # Replace the nans with a 1.0.
+    accuracies = []
+    for i in range(len(total)):
+        if total[i] == 0:
+            accuracies.append(1.0)
+        else:
+            accuracies.append(count[i] / total[i])
+
+    return accuracies
+
+def compute_mean_iou(pred, label):
+
+    unique_labels = np.unique(label)
+    num_unique_labels = len(unique_labels);
+
+    I = np.zeros(num_unique_labels)
+    U = np.zeros(num_unique_labels)
+
+    for index, val in enumerate(unique_labels):
+        pred_i = pred == val
+        label_i = label == val
+
+        I[index] = float(np.sum(np.logical_and(label_i, pred_i)))
+        U[index] = float(np.sum(np.logical_or(label_i, pred_i)))
+
+
+    mean_iou = np.mean(I / U)
+    return mean_iou
+
+
+def evaluate_segmentation(pred, label, num_classes, score_averaging="weighted"):
+    flat_pred = pred.flatten()
+    flat_label = label.flatten()
+
+    global_accuracy = compute_global_accuracy(flat_pred, flat_label)
+    class_accuracies = compute_class_accuracies(flat_pred, flat_label, num_classes)
+
+    prec = precision_score(flat_pred, flat_label, average=score_averaging)
+    rec = recall_score(flat_pred, flat_label, average=score_averaging)
+    f1 = f1_score(flat_pred, flat_label, average=score_averaging)
+
+    iou = compute_mean_iou(flat_pred, flat_label)
+
+    return global_accuracy, class_accuracies, prec, rec, f1, iou
